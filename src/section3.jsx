6ebad2f99d5a8218af00e775198e001e5eb2cc04
RenAverion <averionren82@gmail.com>
2026-03-24 15:16:45 +0800

    Additional content and Adjustments
    - changes in p text size of about section for large screen readability
    - additional content for invitation details

--- src/section3.jsx
+++ src/section3.jsx
@@ -5,10 +5,19 @@ export default function InvitationDetails (){
              <h3 className="text-center font-ad text-xl md:px-0 md:text-3xl lg:text-2xl font-bold text-prim pb-1">Invitation Details</h3>
              <hr className="w-20 h-[2px] my-1 mx-auto bg-gold border-none sr" />
         </div>
-        <div>
-            <div></div>
-            <div></div>
-            <div></div>
+        <div className="flex justify-around p-10 font-ad flex-col md:flex-row">
+            <div className="flex-1">
+              <h3 className="text-center font-ad text-l md:px-0 md:text-xl lg:text-xl font-bold text-prim pb-1">Who?</h3>
+              <p className="font-semibold text-m text-center mt-[5px] mb-[5px] md:mb-0 lg:text-xl ">You and 2 companions are invited</p>
+            </div>
+            <div className="flex-1">
+              <h3 className="text-center font-ad text-l md:px-0 md:text-xl lg:text-xl font-bold text-prim pb-1">When?</h3>
+              <p className="font-semibold text-m text-center mt-[5px] mb-[5px] md:mb-0 lg:text-xl"></p>
+            </div>
+            <div className="flex-1">
+             <h3 className="text-center font-ad text-l md:px-0 md:text-xl lg:text-xl font-bold text-prim pb-1">Where?</h3>
+             <p className="font-semibold text-m text-center mt-[5px] mb-[5px] md:mb-0 lg:text-xl"></p>
+            </div>
         </div>
       </section>   
     );
